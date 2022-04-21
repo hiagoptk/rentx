87edda75bd7b22789ecd47900283c22b4a0aaf97
Hiago Patrick <hiogaptk@gmail.com>
2022-04-21 16:36:29 -0300

    definindo rota para upload do avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateAvaterUseCase/UpdateAvatarController";
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./temp/avatar"));
+
 const createUserControler = new CreateUserController();
+const updateUserAvatarCotroller = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserControler.handle);
+
+usersRoutes.patch(
+    "/avatar",
+    ensureAuthenticated,
+    uploadAvatar.single("avatar"),
+    updateUserAvatarCotroller.handle
+);
 
 export { usersRoutes };
