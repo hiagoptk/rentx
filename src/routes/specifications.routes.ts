006572aecac7c9187195dc507f05e17f9dbe8232
Hiago Patrick <hiogaptk@gmail.com>
2022-04-21 13:42:24 -0300

    aplicando autenticação nas rotas de especificações

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationsController } from "../modules/cars/useCases/listSpecifications/ListSpecificationsController";
 
@@ -7,6 +8,7 @@ const specificationRoutes = Router();
 const createSpeficiationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
+specificationRoutes.use(ensureAuthenticated);
 specificationRoutes.post("/", createSpeficiationController.handle);
 
 specificationRoutes.get("/", listSpecificationsController.handle);
